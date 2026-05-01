e53b3e9f7fc16459350658245c3c89d9029c6fa3
Estevao67 <estevaopauloa763@gmail.com>
2026-04-30 21:39:50 -0400

    Atualizado com implementacao

--- funcoes/subtracao.py
+++ funcoes/subtracao.py
@@ -1,5 +1,11 @@
-def subtrair():
-    subtracao1 = float(input("Insira o primeiro número: "))
-    subtracao2 = float(input("Insira o segundo número: "))
-    return print(subtracao1 - subtracao2)
+def pedir_numero(mensagem):
+    while True:
+        try:
+            return float(input(mensagem))
+        except ValueError:
+            print("Digite um número válido!")
 
+def subtrair():
+    n1 = pedir_numero("Insira o primeiro número: ")
+    n2 = pedir_numero("Insira o segundo número: ")
+    print("Resultado:", n1 - n2)
