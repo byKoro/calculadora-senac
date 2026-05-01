ade4b347ed4168d058b4e8d4adc85ba5af9f01d6
Antonio Abreu <antonio62479146@aluno.ms.senac.br>
2026-04-30 21:33:23 -0400

    Resto de divisão

--- funcoes/Resto da visião.py
+++ funcoes/Resto da visião.py
@@ -1,11 +1,10 @@
 def Resto_de_divisao():
     while True:
         print("Resto de divisão: ")
-        sair = 0
         print("Digite 0 se quiser sair")
         n1 = float(input("Digite o Numero"))
         n2 = float(input("Digite o Numero"))
-        if n1 == 0:
+        if n1 == 0 :
             print("Voltando")
             break
         elif n1 <0 or n2 <0:
@@ -13,5 +12,5 @@ def Resto_de_divisao():
         else:
             resto = n1 % n2
             print(f"Resto da divisao: {resto}")
-            
+            break
 Resto_de_divisao()
