351d5ec4be55b6457c88791c1c1f9cdb0364c984
kaylaineleandra076 <kaylaineleandra076@gmail>
2026-04-30 21:25:28 -0400

    funçao multiplicar corrijida.

--- funcoes/multiplicar.py
+++ funcoes/multiplicar.py
@@ -1,20 +1,26 @@
 
 def multiplicar():
+
     print("=-=-=-=-Multiplicar-=-=-=-=")
 
     while True:
-        
-        n1= float(input("Digite o primeiro número: "))
 
-        n2= float(input("Digite o segundo número: "))
+        try:
+            n1= float(input("Digite o primeiro número: "))
 
-        resultado = n1 * n2
+            n2= float(input("Digite o segundo número: "))
+            
+            resultado = n1 * n2
+            
+            print(f"Resultado: {resultado}")
+            
+            continuar = input("Deseja realizar outra multiplicação? (s/n): ").lower()
 
-        print(f"Resultado: {resultado}")
-        
-        continuar = input("Deseja realizar outra multiplicação? (s/n): ").lower()
+        except ValueError:
+            print("Entrada inválida. Por favor, digite um número válido.")
 
-        if continuar != 's':
-            print("Voltando ao menu principal...")
-            break
-  
+
+            if continuar != 's':
+                
+                print("Voltando ao menu principal...")
+                break
